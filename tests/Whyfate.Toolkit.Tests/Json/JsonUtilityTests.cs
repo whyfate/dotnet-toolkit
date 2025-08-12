2bb49c1aa7c1e372e8de06e78f3e9640c1f68a7e
haowang <593301179@qq.com>
2025-08-12 16:32:31 +0800

    json utility add unitest.

--- tests/Whyfate.Toolkit.Tests/Json/JsonUtilityTests.cs
+++ tests/Whyfate.Toolkit.Tests/Json/JsonUtilityTests.cs
@@ -22,7 +22,7 @@ public class JsonUtilityTests
     }
 
     [Fact]
-    public void TestSerilization()
+    public void TestSerialize()
     {
         var list = new List<string>
         {
@@ -36,6 +36,31 @@ public class JsonUtilityTests
     }
     
     [Fact]
+    public void TestSerializeType()
+    {
+        Person p1 = new Student("1", 35, DateOnly.Parse("2000-01-01"), DateTime.Now, "L1");
+        var json1 = JsonSerializer.Serialize(p1);
+        var json2 = JsonSerializer.Serialize(p1,typeof(Student));
+        Assert.NotNull(json1);
+        Assert.NotNull(json2);
+        Assert.DoesNotContain("L1", json1);
+        Assert.Contains("L1", json2);
+    }
+
+    [Fact]
+    public void TestDeserializeType()
+    {
+        var p1 = new Person("1", 35, DateOnly.Parse("2000-01-01"), DateTime.Now);
+        var json = JsonUtility.Serialize(p1);
+        var p2 = JsonUtility.Deserialize<Person>(json, typeof(Person));
+        Assert.NotNull(p2);
+        Assert.Equal(p1.Id, p2.Id);
+        Assert.Equal(p1.Age, p2.Age);
+        Assert.Equal(p1.BirthDay, p2.BirthDay);
+        Assert.Equal(p1.CreateTime.ToString("yyyy-MM-dd HH:mm:ss"), p2.CreateTime.ToString("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    [Fact]
     public void TestSetOptions()
     {
         JsonSerializerOptionsFactory.Set(new JsonSerializerOptions
@@ -46,14 +71,11 @@ public class JsonUtilityTests
         Assert.NotNull(options);
         Assert.True(options.PropertyNameCaseInsensitive);
     }
-    
+
     [Fact]
     public void TestException()
     {
-        Assert.Throws<ArgumentNullException>(() =>
-        {
-            JsonSerializerOptionsFactory.Set(null!);
-        });
+        Assert.Throws<ArgumentNullException>(() => { JsonSerializerOptionsFactory.Set(null!); });
     }
 }
 
@@ -63,4 +85,9 @@ class Person(string id, int age, DateOnly birthDay, DateTime createTime)
     public int Age { get; } = age;
     public DateOnly BirthDay { get; } = birthDay;
     public DateTime CreateTime { get; } = createTime;
+}
+
+class Student(string id, int age, DateOnly birthDay, DateTime createTime, string grade) : Person(id, age, birthDay, createTime)
+{
+    public string Grade { get; set; } = grade;
 }
